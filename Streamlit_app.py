8a244733bbf70e0d83b372ff48c394fb1a27df44
Amanda Gabriel Silva <amanda.gsilva@unimedmaceio.com.br>
2025-09-12 11:57:09 -0300

    Atualizando Streamlit_app com nova lógica

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,26 +1,22 @@
-import streamlit as st
 import os
+import streamlit as st
 
-PASTA = r'C:\DADOS\ATENDIMENTO'
-USUARIO = 'amanda'
-SENHA = 'senha123'
+PASTA = "ATENDIMENTO"
 
 st.set_page_config(page_title="Arquivos Disponíveis", layout="centered")
 
-# Autenticação simples
 st.title("🔐 Login")
 usuario = st.text_input("Usuário")
 senha = st.text_input("Senha", type="password")
 
+USUARIO = 'amanda'
+SENHA = 'senha123'
+
 if usuario == USUARIO and senha == SENHA:
     st.success("Login bem-sucedido!")
+    st.title("📂 Arquivos disponíveis")
 
-    st.title("📁 Arquivos disponíveis")
     arquivos = os.listdir(PASTA)
-
     for arquivo in arquivos:
-        caminho = os.path.join(PASTA, arquivo)
-        with open(caminho, "rb") as f:
-            st.download_button(label=f"📥 Baixar {arquivo}", data=f, file_name=arquivo)
-else:
-    st.warning("Digite usuário e senha para acessar os arquivos.")
+        st.write(f"- {arquivo}")
+
